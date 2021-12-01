fd2b2c6ed935eb93d68407044910a952c809d12e
Du'An Lightfoot <duanl@labeveryday.com>
2021-12-01 02:07:26 -0600

    Modified init

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from app.auto_hue import AutoHue
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+
+bridgeIpAddress = os.getenv("BRIDGE_IP")
 
 def main():
     """
     Every time script is run lights will change from
     YouTube to Night settings.
     """
-    hue = AutoHue()
+    hue = AutoHue(bridgeIpAddress)
     state = hue.get_all_light_state()
     if state[0]['hue'] == 6291:
         hue.set_office_youtube_lights()
